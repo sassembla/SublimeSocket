680435431253a054001bad3302829e866a7c1dc2
sassembla <toru.inoue@kissaki.tv>
2013-02-05 08:36:39 +0900

    add LISTEN_EVENT

--- SublimeSocketAPISettings.py
+++ SublimeSocketAPISettings.py
@@ -1,15 +1,28 @@
 
 #API for Input to ST2 through WebSocket
+
+### COMMAND = COMMAND_NAME : JSON_EXPRESSION
+### COMMANDS = COMMAND_NAME : JSON_EXPRESSION -> COMMAND_NAME : JSON_EXPRESSION -> ...
+
 API_PREFIX = "sublimesocket"
 API_PREFIX_SUB = "ss"
 
-API_DEFINE_DELIM = "@"
-API_CONCAT_DELIM = "->"
-API_COMMAND_PARAMS_DELIM = ":"# only first ":" will be evaluated as delimiter.
+API_DEFINE_DELIM = "@"					# sublimesocket@commandA:{}->commandB:{}->commandC:[]->
+API_CONCAT_DELIM = "->"					# concat commands. every commands run in sequential.
+API_COMMAND_PARAMS_DELIM = ":"	# only first ":" will be evaluated as delimiter / each commnand.
 
 API_INPUTIDENTITY = "inputIdentity"
 API_KILLSERVER    = "killServer"
+
 API_EVENTLISTEN		= "eventListen"
+LISTEN_EVENTS			= ["on_modified"]
+
+API_SET_KVSTOREEVENT			= "setKVStoreEvent"
+
+# API_RUNSHELL			= "runShell"
+
+API_OUTPUT				= "output"
+
 API_TIMEREVENT		= "timerEvent"
 
 API_EVAL					= "eval"
--- SublimeSocketHandle.py
+++ SublimeSocketHandle.py
@@ -3,27 +3,40 @@ import sublime, sublime_plugin
 from SublimeWSServer import *
 from OpenPreference import *
 
+
+# WebSocket server's thread
+thread = None
+
 class Socketon(sublime_plugin.TextCommand):
   def run(self, edit):
     self.startServer()
 
   @classmethod
   def startServer(self):
+    global thread
+
+    if thread is not None and thread.is_alive():
+      return sublime.message_dialog('SublimeSocket Already Running')
+        
     host = sublime.load_settings("SublimeSocket.sublime-settings").get('host')
     port = sublime.load_settings("SublimeSocket.sublime-settings").get('port')
 
     thread = SublimeSocketThread(host, port)
     thread.start()
 
-  class On_then_openpref(sublime_plugin.TextCommand):
-    def run(self, edit):
-      Socketon.startServer()
-      Openpreference.openSublimeSocketPreference()
-
+class On_then_openpref(sublime_plugin.TextCommand):
+  def run(self, edit):
+    Socketon.startServer()
+    Openpreference.openSublimeSocketPreference()
+      
 class Socketoff(sublime_plugin.TextCommand):
   def run(self, edit):
+    global thread
+    print "thread is,, ", thread.is_alive()
     print "off.... not yet implimented as standalone. Plase use preference > Kill Button"
         
+
+
 # threading
 class SublimeSocketThread(threading.Thread):
   def __init__(self, host, port):
@@ -35,21 +48,19 @@ class SublimeSocketThread(threading.Thread):
   def run(self):
     self._server.start(self._host, self._port)
 
+
+  # send eventName and data to server
+  def toServer(self, eventName):
+    self._server.fireKVStoredEvent(eventName)
+    
+
 # event listeners
 class CaptureEditing(sublime_plugin.EventListener):
   edit_info = {}
   def on_modified(self, view):
-    # 街頭のビューのイベントをハンドルしているかどうか不明だけど、とりあえずserverに伝える
-    vid = view.id()
-    if not isView(vid):
-      # I only want to use views, not 
-      # the input-panel, etc..
-      return
-    if not CaptureEditing.edit_info.has_key(vid):
-      # create a dictionary entry based on the 
-      # current views' id
-      CaptureEditing.edit_info[vid] = {}
-      cview = CaptureEditing.edit_info[vid]
-      # I can now store details of the current edit 
-      # in the edit_info dictionary, via cview.
+    global thread
+
+    if thread is not None and thread.is_alive():
+      thread.toServer("on_modified")
+
 
