7eee0f35820d91f09d338bc75ef0849014a9ba3e
sassembla <toru.inoue@kissaki.tv>
2013-04-09 01:57:58 +0900

    "chromeClient" folder comes back.

---
+++ tool/chromeClient/tscshell.sh
@@ -0,0 +1,19 @@
+export PATH=/usr/local/bin:/usr/bin
+
+#$1 = .ts files array
+#$2 = logFilePath
+
+echo start >> $2
+
+#tsc includes "node" call
+/usr/local/bin/tsc $1 2>> $2
+
+var=`tail -1 $2`
+if [ "$var" == "start" ]
+	then
+	echo typescript compile succeeded. >> $2
+else
+	echo typescript compile failure. >> $2	
+fi
+
+
