b22e2dc43e15624cc748c3d6d1ff363209c716bd
sassembla <toru.inoue@kissaki.tv>
2013-04-08 17:46:09 +0900

    add ScalaClient

---
+++ tool/chromeClients/ScalaClient/SublimeSocketChromeClient.js
@@ -0,0 +1,270 @@
+// 
+// Client of SublimeSocket.
+// 
+
+var ssChromeClient_tailing_tab = null;
+var ssChromeClient_tailing_interval = 0;
+
+var ssChromeClient_read_position = 0;
+var ssChromeClient_current_text = null;
+var ssChromeClient_display_lock = false;
+
+var INTERVAL_TAILING = 500;
+
+
+
+var currentFilterName = "scala";
+var KEY_CLIENT_IDENTITY = "SublimeSocketChromeClient";
+
+
+// settings for TypeScript
+var CURRENT_SETTING_PATH = "SUBLIMESOCKET_PATH:tool/chromeClient/ScalaFilter.txt";
+
+var TSC_SIMPLE_COMPILE_SHELLPATH = "SUBLIMESOCKET_PATH:tool/chromeClients/ScalaClient/tscshell.sh";
+
+var TSC_TYPESCRIPTFILE_WILDCARD = "*.ts";
+var currentTSCompileLogFileName = "";
+
+
+var TARGET_FOCUSED = 0;
+var TARGET_FOLDER = 1;
+var TARGET_RECURSIVE = 2;
+
+var SCALAGRADLEC_COMPILATIONTARGETMODE = TARGET_FOLDER;
+var SCALAGRADLEC_IDENTIFIED_SENDER_STARTMARK = "scalasaved";
+var SCALAGRADLEC_IDENTIFIED_SENDER_ENDMARK = "scalacompilefinished";
+
+var needTail = false;
+
+chrome.browserAction.onClicked.addListener(function(tab){
+    if( ssChromeClient_tailing_tab === null && tab.url.indexOf('file://') == 0){
+        ssChromeClient_tailing_tab = tab;
+        chrome.browserAction.setIcon({path:"images/sublimesocketchromeicon-active.png"});
+
+        needTail = false;        
+        ssChromeClient_tailing_interval = setInterval(checkFile, INTERVAL_TAILING);
+
+        // connect.
+        _WS.init(tab.url, SCALAGRADLEC_COMPILATIONTARGETMODE);
+        _WS.connect();
+    }else{
+        chrome.browserAction.setIcon({path:"images/sublimesocketchromeicon-inactive.png"});
+        clearInterval(ssChromeClient_tailing_interval);
+        ssChromeClient_tailing_tab = null;
+
+        // close
+        _WS.close();
+    }
+})
+
+var _WS = {
+    uri: 'ws://127.0.0.1:8823/',
+
+
+    currentTargetFolderPath:"",
+    currentCompilationTargetMode:-1,
+
+    ws: null,
+
+    init : function (targetLogPath, compilationTargetMode) {
+
+        // replace file path as FileSystem path
+        var logPath = targetLogPath.replace("file:///", "/");
+
+        pathArray = logPath.split("/");
+
+        // last path is "logfile.log"
+        currentTSCompileLogFileName = pathArray[pathArray.length-1];
+        
+        // get FolderPath
+        currentTargetFolderPath = logPath.replace("/"+currentTSCompileLogFileName, "/");
+        console.log("currentTargetFolderPath    "+currentTargetFolderPath);
+
+        // set mode from preferences
+        currentCompilationTargetMode = compilationTargetMode;
+    },
+
+    connect : function (e) {
+      _WS.s = new WebSocket(_WS.uri);
+      _WS.s.onopen = function (e) { _WS.onOpen(e); };
+      _WS.s.onclose = function (e) { _WS.onClose(e); };
+      _WS.s.onmessage = function (e) { _WS.onMessage(e); };
+      _WS.s.onerror = function (e) { _WS.onError(e); };
+    },
+
+    onOpen: function () {
+
+        //call api then get callback
+        _WS.s.send(
+            'sublimesocket@inputIdentity:'+JSON.stringify(
+            {
+                "id":KEY_CLIENT_IDENTITY
+            }
+            )+
+            "->showAtLog:"+JSON.stringify(
+            {
+                "message": "SublimeSocketChromeClient connected to SublimeSocket."
+            }
+            )+
+            "->showStatusMessage:"+JSON.stringify(
+            {
+                "message": "SublimeSocketChromeClient connected to SublimeSocket."
+            }
+            )+
+            "->runSetting:"+JSON.stringify(
+            {
+                "path":CURRENT_SETTING_PATH
+            })
+        );
+    },
+
+    onClose: function (e) {
+        console.log("closed!!? closed nande!?"+e);
+    },
+
+    onMessage: function (e) {
+        if (e.data.indexOf(SCALAGRADLEC_IDENTIFIED_SENDER_STARTMARK) === 0) {
+            
+            var currentCompileTargetFileName = e.data.replace(SCALAGRADLEC_IDENTIFIED_SENDER_STARTMARK+":","")
+            if (currentCompileTargetFileName.indexOf(currentTargetFolderPath) !== -1) {
+                // exist in target folder.
+            } else {
+                return;
+            }
+
+            var runShellJSON = "";
+
+            switch (currentCompilationTargetMode) {
+                case TARGET_FOCUSED:
+                    
+                    runShellJSON = {
+                        "main": "/bin/sh",
+                        "":[
+                            TSC_SIMPLE_COMPILE_SHELLPATH,
+                            currentCompileTargetFileName,
+                            currentTargetFolderPath + currentTSCompileLogFileName
+                        ]
+                    };
+
+                    break;
+                case TARGET_FOLDER:
+                    console.log("TARGET_FOLDER here");
+                    runShellJSON = {
+                        "main": "/bin/sh",
+                        "":[
+                            TSC_SIMPLE_COMPILE_SHELLPATH,
+                            currentTargetFolderPath + TSC_TYPESCRIPTFILE_WILDCARD,
+                            currentTargetFolderPath + currentTSCompileLogFileName
+                        ]
+                    };
+
+                    break;
+                case TARGET_RECURSIVE:
+                    runShellJSON = {
+                        "main": "/bin/sh",
+                        "":[
+                            TSC_RECURSIVE_COMPILE_SHELLPATH,
+                            currentTargetFolderPath + TSC_TYPESCRIPTFILE_WILDCARD,
+                            currentTargetFolderPath + currentTSCompileLogFileName
+                        ]
+                    };
+                    break;
+            } 
+
+            needTail = true;
+            
+            var command = "ss@runShell:"+JSON.stringify(runShellJSON);
+
+            console.log("TARGET_FOLDER here command "+command);
+
+            _WS.s.send(command);
+        }
+
+        if (e.data.indexOf(SCALAGRADLEC_IDENTIFIED_SENDER_ENDMARK) === 0) {
+            needTail = false;
+        }
+
+    },
+
+    onError: function (e) {
+        console.log("error:"+e.error);
+    },
+
+
+    send: function (message) {
+        if (!message.length) {
+            alert('Empty message not allowed !');
+        } else {
+            _WS.s.send(message);
+        }
+    },
+    close: function () {
+        console.log("close!!");
+        _WS.s.close();
+    }
+};
+
+
+
+function checkFile(){
+    if( ssChromeClient_tailing_tab == null ){
+        return;
+    }
+
+    if (!needTail) {
+        return;
+    }
+
+    chrome.tabs.reload(ssChromeClient_tailing_tab.id);
+    
+    chrome.tabs.executeScript(
+        ssChromeClient_tailing_tab.id, {code:'document.body.innerText'}, function(result){
+            if( ssChromeClient_display_lock == true ){
+                return;
+            }
+
+            if( ssChromeClient_current_text !== null ){
+              ssChromeClient_current_text = result[0].substr(ssChromeClient_read_position);
+            }else{
+                ssChromeClient_current_text = '';
+            }
+
+            ssChromeClient_read_position = result[0].length;
+            if( ssChromeClient_current_text == '' ){
+                return;
+            }
+            chrome.windows.getAll(
+                function(windows){
+                    for(var i = 0; i < windows.length; i++){
+                        chrome.tabs.getAllInWindow(
+                            window.id, function(tabs){
+                                for( var j = 0; j < tabs.length; j++ ){
+                                    if( tabs[j].url.indexOf('file') == 0 || tabs[j].url.indexOf('http') == 0 ){
+                                        if( ssChromeClient_current_text != '' ){
+
+                                            var lines = ssChromeClient_current_text.split("\n");
+
+                                            for (var i = 0; i < lines.length; i++) {
+                                                console.log("line "+lines[i]);
+                                                filteringJSON = {
+                                                    "name":currentFilterName,
+                                                    "source":lines[i]
+                                                };
+
+                                                console.log("ssChromeClient_current_text:"+ssChromeClient_current_text);
+                                                _WS.s.send("ss@filtering:"+JSON.stringify(filteringJSON));
+                                            }
+                                        }
+                                    }
+                                }
+                                ssChromeClient_current_text = '';
+                                ssChromeClient_display_lock = false;
+                            }
+                        )
+                    }
+                }
+            )
+        }
+    );
+}
+
